20363558e316e869ab94d89deae49e6d23aefdd3
swolebrain <thecodingteacher@gmail.com>
2017-08-06 19:26:43 -0400

    feat: v1

--- js/main.js
+++ js/main.js
@@ -2,18 +2,39 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight * 0.8;
+var GAME_STATE = "running";
 
+var detectCollisions = require('./detectCollisions.js');
 var player = require('./player.js');
 var scene = require('./scene.js');
+var Box = require('./Obstacles/Box.js');
+var boxes = [];
+
+function generateBox(){
+  boxes.push(new Box(100, 100, 10, window.innerWidth, canvas.height -180  ));
+  if (GAME_STATE == "running")
+    setTimeout(generateBox,  Math.ceil(Math.random()*3)*1000);
+}
+generateBox();
 
 function gameLoop(){
   ctx.clearRect(0,0,canvas.width, canvas.height);
-  //update de todo
   player.update();
-  //render de todo
+  boxes.forEach(function(box){
+    box.update();
+    var thereWasACollision = detectCollisions(player, box);
+    if (thereWasACollision) {
+      GAME_STATE = "dead";
+    }
+  });
+
   scene.render(ctx, canvas);
   player.render(ctx);
-  window.requestAnimationFrame(gameLoop);
+  boxes.forEach(function(box){
+    box.render(ctx);
+  })
+  if (GAME_STATE == "running")
+    window.requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
